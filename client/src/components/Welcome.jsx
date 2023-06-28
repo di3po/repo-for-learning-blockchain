c1d8321aded3491750cc3fb9f7451fbdfc630ca2
di3po <bolatbekd1n@gmail.com>
2023-06-28 17:11:54 +0600

    tested 2 cho tams: preconnection of back and front

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,7 +1,9 @@
+import React, { useContext } from 'react';
 import { AiFillPlayCircle } from 'react-icons/ai';
 import { SiEthereum } from 'react-icons/si';
 import { BsInfoCircle } from 'react-icons/bs';
 import { Loader } from './';
+import { TransactionContext } from '../context/TransactionContext';
 
 const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
@@ -17,6 +19,8 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
 )
 
 const Welcome = () => {
+    const { value } = useContext(TransactionContext);
+    console.log('че там from Welcome: ', value)
 
     const connectWallet = () => {
 
@@ -112,7 +116,7 @@ const Welcome = () => {
                         />
                         <div className='h-[1px] w-full bg-gray-400 my-2' />
                         {
-                            true
+                            false
                             ? (<Loader />)
                             : (
                                 <button
---
+++ client/src/context/TransactionContext.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+import { ethers } from 'ethers';
+import { contractABI, contractAddress } from '../utils/constants';
+
+export const TransactionContext = React.createContext();
+
+const { ethereum } = window;
+
+const getEthereumContract = () => {
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    const signer = provider.getSigner();
+    const transactionContract = new ethers.Contract({contractAddress, contractABI, signer});
+
+    console.log({provider, signer, transactionContract});
+
+}
+
+export const TransactionProvider = ({ children }) => {
+    const checkIfWalletIsConnected = async () => {
+        if(!ethereum) alert('Please, install Metamask');
+
+        const accounts = await ethereum.request({method: 'eth_accounts'});
+
+        console.log('че там from root: ', accounts)
+
+    }
+    
+    //  runs only at the start
+    useEffect(() => {
+        checkIfWalletIsConnected();
+    }, [])
+
+    return (
+        <TransactionContext.Provider value={{ value: 'dinotest' }}>
+            { children }
+        </TransactionContext.Provider>
+    );
+}
